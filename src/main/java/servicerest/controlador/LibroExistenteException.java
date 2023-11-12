194cced0b637b81ac8be869d85dbb856b5cbb682
Jorge Ruiz Martinez <joruizma@emeal.nttdata.com>
2023-11-12 16:59:32 +0100

    Add javadoc archivo LibroExistenteException

--- src/main/java/servicerest/controlador/LibroExistenteException.java
+++ src/main/java/servicerest/controlador/LibroExistenteException.java
@@ -1,6 +1,22 @@
+/**
+ * Excepción personalizada que se lanza cuando se intenta agregar un libro que ya existe.
+ * Extiende la clase RuntimeException para indicar que es una excepción no verificada.
+ *
+ * @author Jorge Ruiz Martinez
+ * @version 1.0
+ */
 package servicerest.controlador;
 
+/**
+ * Excepción lanzada cuando se intenta agregar un libro que ya existe en el sistema.
+ */
 public class LibroExistenteException extends RuntimeException {
+
+    /**
+     * Constructor que acepta un mensaje para describir la excepción.
+     *
+     * @param mensaje Mensaje descriptivo de la excepción.
+     */
     public LibroExistenteException(String mensaje) {
         super(mensaje);
     }
